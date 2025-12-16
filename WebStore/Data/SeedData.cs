1431690ff371e2e3401493f4632bf925ba9df5a7
106010478 <106010478@INTELT-20>
2025-12-16 13:26:56 +0300

    Добавьте файлы проекта.

---
+++ WebStore/Data/SeedData.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebStore.Data
+{
+    public class SeedData
+    {
+        public static async Task Initialize(
+            ApplicationDbContext context,
+            UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            await context.Database.EnsureCreatedAsync();
+
+            if (!await roleManager.RoleExistsAsync("Admin"))
+            {
+                await roleManager.CreateAsync(new IdentityRole("Admin"));
+            }
+
+            var adminEmail = "admin@store.com";
+            var adminUser = await userManager.FindByEmailAsync(adminEmail);
+
+            if (adminUser == null)
+            {
+                adminUser = new IdentityUser
+                {
+                    UserName = adminEmail,
+                    Email = adminEmail,
+                    EmailConfirmed = true
+                };
+                var result = await userManager.CreateAsync(adminUser, Admin)
+        }
+    }
+}
